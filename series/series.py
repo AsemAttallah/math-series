629d81a75b6479076ac983dbdfb9956059f41312
AsemAttallah <attallahasem@gmail.com>
2023-05-05 17:52:21 +0000

    class05

--- series/series.py
+++ series/series.py
@@ -13,4 +13,30 @@ def lucas(n):
         return 1
     else :
         return lucas(n-1)+lucas(n-2)
-        
+
+
+def sum_series(n,x=0,y=1):
+    if x==0 and y==1:
+        if n==0:
+            return 0
+        if n==1:
+            return 1
+        else :
+            return sum_series(n-1)+sum_series(n-2)
+    
+    # if x==2 and y==1:
+    #     if n==0:
+    #         return 2
+    #     if n==1:
+    #         return 1
+    #     else :
+    #         return sum_series(n-1,2,1)+sum_series(n-2,2,1)
+    
+    if x!=0  or y!=1:
+        if n==0:
+            return x
+        if n==1:
+            return y
+        else :
+            return sum_series(n-1,x,y)+sum_series(n-2,x,y)
+    
